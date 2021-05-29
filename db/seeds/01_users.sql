17fbf86cebc5f5e07ff433b0eff862488dd25393
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-05-29 04:07:18 +0000

    First commit for CecilTinkering branch

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,20 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name,password,email)
+VALUES
+('Alice','AlicePassword','123@123'),
+('Bob','KiraPassword','abc@abc');
+INSERT INTO maps (name,description)
+VALUES
+('Vancouver weather','rainy'),
+('Edmonton weather','sunny');
+INSERT INTO favourite_maps (user_id,map_id)
+VALUES
+(1,2),
+(2,1);
+INSERT INTO
+pins (title,
+description,
+image,
+map_id,user_id,latitude,longitude)
+VALUES
+('position1','I dont know where it is 1',':hankey:',2,1,49.2609, -123.1140),
+('position2','I dont know where it is 2',':hankey:',1,2,49.260727, -123.126545);
