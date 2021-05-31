506bf3f0476598f7d9341a1beb8d7800dab134cb
JIAQI13 <jiaqi11@ualberta.ca>
2021-05-31 18:54:00 +0000

    add map reset for view single map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -53,6 +53,58 @@ const addMap = (req) => {
 // }
 
 $(document).ready(() => {
+  //create new popups
+  const createNewPopUps = (maps) => {
+    const $popups = `
+          <div id='popupTitle'>
+            <p>this is a title</p>
+          </div>
+          <div id='popupImage'>
+            <p>Image goes here</p>
+          </div>
+          <div id='popupDescription'>
+            <p>this is a description</p>
+          </div>
+          <div id='popupButtons'>
+            <form method="POST">
+              <label for="title-input">Title:</label>
+              <input id="title-input" type="text" name='title'><br>
+              <label for="description-input">Description:</label>
+              <input id="description-input" type="text" name=description'>
+              <input type="file"  accept="image/*" name="image" id="file"><br>
+              <input type='submit' value='Edit' class='editButton'/>
+              </form>
+            </div>
+          <div >
+            <input type='button' value='Delete' class='deleteButton'/>
+          </div>
+            `;
+    return $popups;
+  }
+
+  //  NEW MAP
+  const mymap = L.map("mapid").setView([51.505, -0.09], 13);
+  const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+  const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+  const tiles = L.tileLayer(tileUrl, { attribution });
+  tiles.addTo(mymap);
+  // CREATE MARKER WHEREVER YOU CLICK
+  mymap.on('click', function (e) {
+    let marker = new L.marker(e.latlng).addTo(mymap);
+    marker.bindPopup(createNewPopUps());
+    marker.on("popupopen", onPopupOpen);
+    return marker;
+  });
+
+  // DELETE BUTTON FUNCTIONALITY
+  function onPopupOpen() {
+    let tempMarker = this;
+    // To remove marker on click of delete button in the popup of marker
+    $(".deleteButton:visible").click(function () {
+      mymap.removeLayer(tempMarker);
+    });
+  }
+
 
   //login is handle by the html
 
@@ -84,6 +136,7 @@ $(document).ready(() => {
         $(`<div>`).html(`name: ${result.maps[0].name}`).appendTo($("#edit-map"));
         $(`<div>`).html(`description: ${result.maps[0].description}`).appendTo($("#edit-map"));
         $(`<input type='submit' value='Edit' class='editButton'>`).appendTo($("#edit-map"));
+        mymap.setView([0, 0], 0);
       });
   });
 
