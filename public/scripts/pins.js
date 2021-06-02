5bbd5af14de92cd01dc8b46fe5505b34c486b66c
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-06-02 23:10:20 +0000

    Add Choose File button that renders the image uploaded right into popup. CANNOT SAVE IT TO DATABASE YET

--- public/scripts/pins.js
+++ public/scripts/pins.js
@@ -1,21 +1,57 @@
 //create new popups for a new pin
+// const createNewPopUps = (title, image, description, lat, lng) => {
+//   const $popups = `
+//         <div id='pin-pop'>
+//         <img id='output' src="/images/Default.png"><br>
+//         <input id='popupTitle' value='${title}'>
+//         <input id='popupImage' value='${image}' type="file"  accept="image/*" name="image">
+//         <input id='popupDescription' value='${description}'>
+//         <input id='lat' value='${lat}' type='hidden'>
+//         <input id='lng' value='${lng}' type='hidden'>
+//         <div id='popupButtons'>
+//             <input type='submit' value='Edit' class='editButton'>
+//             <input type='button' value='Delete' class='deleteButton'>
+//         </div>
+//         </div>
+//           `;
+//   return $popups;
+// }
+
+
+
 const createNewPopUps = (title, image, description, lat, lng) => {
   const $popups = `
         <div id='pin-pop'>
-        <img src="/images/Default.png"><br>
         <input id='popupTitle' value='${title}'>
-        <input id='popupImage' value='${image}' type="file"  accept="image/*" name="image">
+        <img id="output" width="200" height="200" src="/images/Default.png" />
         <input id='popupDescription' value='${description}'>
+        <input type="file"  accept="image/*" name="images" id="file"  onchange="loadFile(event)">
         <input id='lat' value='${lat}' type='hidden'>
         <input id='lng' value='${lng}' type='hidden'>
         <div id='popupButtons'>
-            <input type='submit' value='Edit' class='editButton'>
-            <input type='button' value='Delete' class='deleteButton'>
+        <input type='submit' value='Edit' class='editButton'>
+        <input type='button' value='Delete' class='deleteButton'>
         </div>
         </div>
-          `;
-  return $popups;
-}
+        `;
+        return $popups;
+      }
+
+
+
+
+
+
+{/* <p><input type="file"  accept="image/*" name="image" id="file"  onchange="loadFile(event)" style="display: none;"></p>
+  <p><label for="file" style="cursor: pointer;">Upload Image</label></p>
+  <p><img id="imageSpot" width="200" /></p>
+
+  <script>
+  var loadFile = function(event) {
+    var image = document.getElementById('imageSpot');
+    image.src = URL.createObjectURL(event.target.files[0]);
+  };
+  </script> */}
 
 //add a marker to db
 const addMarkerToDB = (mapID, userID, pinLat, pinLng) => {
