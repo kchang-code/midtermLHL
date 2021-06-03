f0365dc089ae081c4e7ac7bda8379fd7c0d59d3b
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-06-03 00:34:12 +0000

    Commit before pull

--- public/scripts/pins.js
+++ public/scripts/pins.js
@@ -40,19 +40,6 @@ const createNewPopUps = (title, image, description, lat, lng) => {
 
 
 
-
-
-{/* <p><input type="file"  accept="image/*" name="image" id="file"  onchange="loadFile(event)" style="display: none;"></p>
-  <p><label for="file" style="cursor: pointer;">Upload Image</label></p>
-  <p><img id="imageSpot" width="200" /></p>
-
-  <script>
-  var loadFile = function(event) {
-    var image = document.getElementById('imageSpot');
-    image.src = URL.createObjectURL(event.target.files[0]);
-  };
-  </script> */}
-
 //add a marker to db
 const addMarkerToDB = (mapID, userID, pinLat, pinLng) => {
   $.ajax({
